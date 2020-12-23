00fd9bb2d2d48956e852c1328d50ed7838077395
Виталий Рощупкин <vroshchupkin@Magnum.local>
2020-12-23 16:00:12 +0300

    ending modal window

--- src/js/plugins/modal.js
+++ src/js/plugins/modal.js
@@ -1,3 +1,41 @@
-$.modal = function() {
-  
+function _createModal(щзешщты) {
+  const modal = document.createElement('div');
+  modal.classList.add('v-modal');
+  modal.insertAdjacentHTML(
+    'afterbegin',
+    ` <div class="v-modal__overlay">
+      <div class="v-modal__window">
+        <div class="v-modal__header">
+          <span class="v-modal__title">Modal title</span>
+          <span class="v-modal__close">&times;</span>
+        </div>
+        <div class="v-modal__body">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="v-modal__footer">
+          <button class="btn btn-success">Ok</button>
+          <button class="btn btn-danger">Cancel</button>
+        </div>
+      </div>
+    </div>
+  `
+  );
+  document.body.appendChild(modal);
+  return modal;
 }
+
+$.modal = function (options) {
+  const $modal = _createModal(options);
+  return {
+    open() {
+      $modal.classList.add('open');
+    },
+    close() {
+      $modal.classList.remove('open');
+      $modal.classList.add('hidden');
+      setTimeout($modal.classList.remove('hidden'), 300);
+    },
+    destroy() {},
+  };
+};
